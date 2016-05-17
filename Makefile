176c36a77a44dee71b95db8f13d1c5caced585f6
LouisLin <a9310139@gmail.com>
2016-05-17 13:45:37 +0800

    add doubly_linklist

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
 LIBS = -lm
 OBJS = single_linklist.o
 OBJS1 = circular_linklist.o
-main: $(OBJS) $(OBJS1)
+OBJS2 = doubly_linklist.o
+main: $(OBJS) $(OBJS1) $(OBJS2)
 	gcc -o single_linklist $(OBJS) $(LIBS)
 	gcc -o circular_linklist $(OBJS1) $(LIBS)
+	gcc -o doubly_linklist $(OBJS2)
 clean: 
-	rm -f single_linklist circular_linklist $(OBJS) $(OBJS1)
+	rm -f single_linklist circular_linklist doubly_linklist $(OBJS) $(OBJS1) $(OBJS2)
 	
---
+++ doubly_linklist.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//create Node structure
+typedef struct node{
+	int data;
+	struct node *prev;
+	struct node *next; //pointer to next node
+}NODE;
+
+NODE *head = NULL;
+NODE *tail = NULL;
+
+void Insert_Node(int data); 
+int Length(NODE *temp);
+void Print_List(void);
+
+int main(){
+	
+	printf("Linklist First Program\n");
+	//create_Node(1);
+	Insert_Node(50);
+	Insert_Node(10);
+	Insert_Node(20);
+	Insert_Node(26);
+	Print_List();	
+	return 0; 
+
+}
+
+void Insert_Node(int data)
+{
+	NODE *new_Node =(NODE *) malloc(sizeof(NODE));
+	
+	if (new_Node == NULL)//can't allocate memory
+		printf("memory is not enough");	
+	
+	new_Node -> data = data;
+	new_Node -> prev = NULL;
+	new_Node -> next = NULL;//next will link to head for circular linklist
+
+	if (head == NULL){//The linklist don't have any value if head is null
+		head = new_Node;
+		tail = new_Node;
+
+	}else{
+		
+		tail -> next = new_Node;
+		new_Node -> prev = tail;
+		tail = new_Node;
+				
+	}
+		
+}
+
+int Length(NODE *temp)
+
+{
+	int count = 0;
+	while(temp != NULL){
+		count++;
+		temp = temp -> next;
+	}
+	return count;
+}
+
+void Print_List(void)
+{
+	printf("Show LinkList Content....\n");
+	
+	NODE *temp = head;
+	
+	while(temp != NULL){
+		printf("%d -->  ", temp->data);
+		temp = temp->next;
+		
+	}
+	printf("NULL\n");
+	
+	temp = tail;
+	while(temp != NULL){
+		printf("%d --> ", temp->data);
+		temp = temp->prev;
+	}
+	printf("NULL\n");
+	printf("Show LinkList Length = %d\n", Length(head));
+
+}
